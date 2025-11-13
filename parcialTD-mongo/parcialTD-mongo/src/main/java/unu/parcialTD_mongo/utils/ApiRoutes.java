eccf18f88755f10d744e5f44078d483f0631fce5
jhonatancg <jhonatan.jesuscg@gmail.com>
2025-11-13 18:23:58 -0500

    second commit

--- parcialTD-mongo/parcialTD-mongo/src/main/java/unu/parcialTD_mongo/utils/ApiRoutes.java
+++ parcialTD-mongo/parcialTD-mongo/src/main/java/unu/parcialTD_mongo/utils/ApiRoutes.java
@@ -8,7 +8,7 @@ public class ApiRoutes {
         }
 
         public static final String BASE = "/api/entidads";
-        public static final String add = "/add";
+        public static final String add = "/login";
         public static final String getById = "/{id}";
         public static final String edit = "/edit/{id}";
         public static final String delete = "/delete/{id}";
--- parcialTD-mysql/parcialTD-mysql/src/main/java/unu/parcialTD_mysql/utils/ApiRoutes.java
+++ parcialTD-mysql/parcialTD-mysql/src/main/java/unu/parcialTD_mysql/utils/ApiRoutes.java
@@ -8,9 +8,6 @@ public class ApiRoutes {
         }
 
         public static final String BASE = "/api/entidads";
-        public static final String add = "/add";
-        public static final String getById = "/{id}";
         public static final String edit = "/edit/{id}";
-        public static final String delete = "/delete/{id}";
     }
 }
